19f2876a161bb172b7c439e1a48fd329803fd1cb
K.Phaneendra Saketh <sakethphaneendra@gmail.com>
2025-02-06 23:48:06 +0530

    JavaScript Basis Recap

---
+++ DataTypes.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b= "Hello"
+let c = true
+let d= null
+let e =undefined
+
+console.log(a,b,c,d,e)
+
+let arr = [10,20,304,50, "Saketh"]
+console.log(arr, arr[2])
+
+let obj = {Name : "Saketh", RollNO : 12, age : 21};
+let obj3 = {Name : "Neha", RollNO : 13, age : 22};
+
+var obj2 = {obj : obj2, Name : "JayaSree", RollNO:14, age : 21}
+console.log(obj2)
---
+++ variables.js
@@ -0,0 +1,8 @@
+let a = 12;
+var b = 13;
+const c = 14;
+s();
+function s(){
+   
+    console.log(a);
+}
